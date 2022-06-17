de8a674ea94c95418a6867e011f898c783226d15
Артём <artemkorsakov@mail.ru>
2022-06-17 08:32:13 +0300

    Scaladoc

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,12 +4,12 @@ import sbt._
 object Dependencies {
 
   object Version {
-    val Scala3 = "3.1.2"
+    val Scala3 = "3.1.3"
 
     val munit = "0.7.29"
   }
 
-  val workbook = Seq(
+  val workbook: Seq[ModuleID] = Seq(
     "org.scalameta" %% "munit" % munit % Test,
     "org.scalameta" %% "munit-scalacheck" % munit % Test
   )
